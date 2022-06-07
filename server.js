27f5565d923815785a7ff1f50d5bbdd9da8c7bd2
Omari Archer <omari.archer21@gmail.com>
2022-06-07 17:37:29 -0400

    wine app

---
+++ server.js
@@ -0,0 +1,85 @@
+
+////Dependencies
+require("dotenv").config();
+const express = require ("express");
+const app = express();
+const cors = require("cors")
+const morgan = require ("morgan");
+const mongoose = require ("mongoose");
+const Wine = require("./models/wine");
+const { PORT = 3001, DATABASE_URL } = process.env
+
+
+
+///Middleware
+app.use(cors())
+app.use(morgan("dev"))
+app.use(express.json())
+
+
+
+
+mongoose.connect(DATABASE_URL)
+// Connection Events
+mongoose.connection
+.on("open", () => console.log("You are connected to MongoDB"))
+.on("close", () => console.log("You are disconnected from MongoDB"))
+.on("error", (error) => console.log(error))
+//Routes
+
+
+
+//Index Route
+app.get("/", (req,res)=>{
+    res.send("The Vinyard")
+})
+
+app.get("/vinyard", async (req, res) => {
+    try {
+    
+      res.json(await Wine.find({}))
+    } catch (error) {
+      
+      res.status(400).json(error)
+    }
+  })
+  
+  //  CREATE ROUTE
+  app.post("/wine", async (req, res) => {
+    try {
+      
+      res.json(await Wine.create(req.body))
+    } catch (error) {
+    
+      res.status(400).json(error)
+    }
+  })
+  
+  //  DELETE ROUTE
+  app.delete("/wine/:id", async (req, res) => {
+    try {
+      
+      res.json(await Wine.findByIdAndDelete(req.params.id))
+    } catch (error) {
+      
+      res.status(400).json(error)
+    }
+  })
+  
+  //  UPDATE ROUTE
+  app.put("/wine/:id", async (req, res) => {
+    try {
+      
+      res.json(
+        await Wine.findByIdAndUpdate(req.params.id, req.body, { new: true })
+      )
+    } catch (error) {
+    
+      res.status(400).json(error)
+    }
+  })
+
+
+app.listen(PORT, ()=>{
+    console.log(`The vinyard on PORT ${PORT}`)
+})
