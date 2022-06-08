3232be0aedbc9d745a402b97604ce51e62d29e01
Omari Archer <omari.archer21@gmail.com>
2022-06-07 23:38:32 -0400

    updated data

--- models/wineSeedData.js
+++ models/wineSeedData.js
@@ -1,8 +1,8 @@
 module.exports = [
   {
     name: "Dominus Estate",
-    type: "Château Pichon Longueville Lalande",
-    year: "2018",
+    type: "White",
+    year: 2018,
     price: 269,
     details:
       "This is tightly packed, with sleek, pure, driven notes of cassis, plum reduction and blackberry puree allied to an iron spine, while subtle alder, juniper, sweet bay leaf and tobacco notes peek in throughout. Very fine-grained, but impressively sturdy in feel, with a long, lingering note of iron piercing the finish. For the cellar. Cabernet Sauvignon, Cabernet Franc and Petit Verdot. Best from 2024 through 2040. 4,000 cases made. —JM",
@@ -11,8 +11,8 @@ module.exports = [
   },
   {
     name: "Château Pichon Longueville Lalande",
-    type: "String",
-    year: "2018",
+    type: "Red",
+    year: 2017,
     price: 198,
     details:
       "Offers a deep well of dark currant, blackberry paste and plum preserves fruit that needs time to unwind fully, as it’s shrouded in warm earth, tobacco, singed cedar, sweet bay leaf and savory notes. A ramrod of graphite adds to the strident, structured feel. Cabernet Sauvignon, Merlot, Cabernet Franc and Petit Verdot. Best from 2030 through 2050. 13,000 cases made. —JM",
@@ -21,8 +21,8 @@ module.exports = [
   },
   {
     name: "Merum Priorati",
-    type: "Brunello di Montalcino",
-    year: "2016",
+    type: "White",
+    year: 2019,
     price: 98,
     details:
       "Refined and elegant, this concentrated red shows cherry, violet and anise notes, with expressive mineral undertones that give it finesse. Elements of mountain herb and spice echo through the finish. Grenache, Carignan and Syrah. Drink now through 2033. 14,000 cases made, 1,600 cases imported. —GS",
@@ -31,8 +31,8 @@ module.exports = [
   },
   {
     name: "Poggio Landi",
-    type: "Brunello di Montalcino",
-    year: "2004",
+    type: "Red",
+    year: 2008,
     price: 60,
     details:
       "Broad and mouthfilling, exuding floral, black cherry, blackberry, graphite and stony mineral flavors. Powerful, yet reined in by the vivid acidity, with tannins that are refined and well-integrated. Shows terrific balance and length. Best from 2025 through 2048. 6,000 cases made, 1,000 cases imported.",
